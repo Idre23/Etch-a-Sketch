bbb0b3f4b980cd5ef5d15998a7bd84768b51db0e
idre <bedadu@hotmail.com>
2025-08-05 14:20:06 +0200

    finished

--- javascript/script.js
+++ javascript/script.js
@@ -17,5 +17,28 @@ changeSquare.forEach(square => {
 
 const resetbutton = document.querySelector("#resetBtn")
 resetbutton.addEventListener("click", () => {
-    alert("Please enter a number between 1 and 100:")
+    let gridSize = prompt("Please enter a number between 1 and 100:");
+    createGrid(gridSize);
 })
+
+function createGrid(gridSize) {
+    divContainer.innerHTML = "";
+    if(gridSize < 1 || gridSize > 100 || isNaN(gridSize)) {
+        alert("Try a number between 1 and 100");
+        return;
+    }
+    for(let i = 0; i < gridSize * gridSize; i ++) {
+        const square = document.createElement("div");
+        square.classList.add("grid-square");
+        divContainer.appendChild(square);
+        let squareSize = 960 / gridSize;
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+    }
+    const changeSquare = document.querySelectorAll(".grid-square")
+    changeSquare.forEach(square => {
+    square.addEventListener("mouseenter", () => {
+        square.style.backgroundColor = "black";
+    })
+})
+}
